d5bde8dd7e587270f30d19d24770edd86fd059f1
Seinly <macbookpro@MacBooks-MacBook-Pro.local>
2023-12-03 21:58:33 +0700

    Update Commit

---
+++ PerpustakaanAppMVC/Model/Context/DbContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerpustakaanAppMVC.Model.Context
+{
+    internal class DbContext : IDisposable
+    {
+        // deklarasi private variabel / field
+        private SQLiteConnection _conn;
+        // deklarasi property Conn (connection), untuk menyimpan objek koneksi
+        public SQLiteConnection Conn
+        {
+            get { return _conn ?? (_conn = GetOpenConnection()); }
+        }
+        // Method untuk melakukan koneksi ke database
+
+
+        private SQLiteConnection GetOpenConnection()
+        {
+            string dbName = @"C:\Users\user\Documents\Database PemrogLanjut\DbPerpustakaan.db";
+            string connectionString = $"Data Source={dbName};Version=3;FailIfMissing=True;";
+
+            SQLiteConnection conn = new SQLiteConnection(connectionString);
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print($"Open Connection Error: {ex.Message}");
+                throw; // Rethrow the exception to indicate connection failure
+            }
+
+            return conn;
+        }
+
+        // Method ini digunakan untuk menghapus objek koneksi dari memory ketika sudah tidak digunakan
+        public void Dispose()
+        {
+            if (_conn != null)
+            {
+                try
+                {
+                    if (_conn.State != ConnectionState.Closed) _conn.Close();
+                }
+                finally
+                {
+                    _conn.Dispose();
+                }
+            }
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ PerpustakaanAppMVC/View/FrmMahasiswa.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace PerpustakaanAppMVC.View
+{
+    partial class FrmMahasiswa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.btnCari = new System.Windows.Forms.Button();
+            this.lvwMahasiswa = new System.Windows.Forms.ListView();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnPerbaiki = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnSelesai = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cari Nama Mahasiswa";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(206, 33);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(815, 26);
+            this.txtNama.TabIndex = 1;
+            // 
+            // btnCari
+            // 
+            this.btnCari.Location = new System.Drawing.Point(1048, 30);
+            this.btnCari.Name = "btnCari";
+            this.btnCari.Size = new System.Drawing.Size(104, 35);
+            this.btnCari.TabIndex = 2;
+            this.btnCari.Text = "Cari";
+            this.btnCari.UseVisualStyleBackColor = true;
+            this.btnCari.Click += new System.EventHandler(this.btnCari_Click);
+            // 
+            // lvwMahasiswa
+            // 
+            this.lvwMahasiswa.HideSelection = false;
+            this.lvwMahasiswa.Location = new System.Drawing.Point(38, 83);
+            this.lvwMahasiswa.Name = "lvwMahasiswa";
+            this.lvwMahasiswa.Size = new System.Drawing.Size(1114, 796);
+            this.lvwMahasiswa.TabIndex = 3;
+            this.lvwMahasiswa.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(38, 892);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(111, 37);
+            this.btnTambah.TabIndex = 4;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // btnPerbaiki
+            // 
+            this.btnPerbaiki.Location = new System.Drawing.Point(173, 892);
+            this.btnPerbaiki.Name = "btnPerbaiki";
+            this.btnPerbaiki.Size = new System.Drawing.Size(111, 37);
+            this.btnPerbaiki.TabIndex = 5;
+            this.btnPerbaiki.Text = "Perbaiki";
+            this.btnPerbaiki.UseVisualStyleBackColor = true;
+            this.btnPerbaiki.Click += new System.EventHandler(this.btnPerbaiki_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(310, 892);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(111, 37);
+            this.btnHapus.TabIndex = 6;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // btnSelesai
+            // 
+            this.btnSelesai.Location = new System.Drawing.Point(1041, 892);
+            this.btnSelesai.Name = "btnSelesai";
+            this.btnSelesai.Size = new System.Drawing.Size(111, 37);
+            this.btnSelesai.TabIndex = 7;
+            this.btnSelesai.Text = "Selesai";
+            this.btnSelesai.UseVisualStyleBackColor = true;
+            this.btnSelesai.Click += new System.EventHandler(this.btnSelesai_Click);
+            // 
+            // FrmMahasiswa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1201, 941);
+            this.Controls.Add(this.btnSelesai);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnPerbaiki);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.lvwMahasiswa);
+            this.Controls.Add(this.btnCari);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "FrmMahasiswa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data Mahasiswa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.Button btnCari;
+        private System.Windows.Forms.ListView lvwMahasiswa;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnPerbaiki;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnSelesai;
+    }
+}
